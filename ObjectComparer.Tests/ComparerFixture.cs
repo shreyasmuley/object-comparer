89a3769bb52c808ddd7719f0a11e1e0d7db186a6
Shreyas Muley <shreyasmuley@gmail.com>
2021-01-12 23:30:30 +0530

    Initial checkin

---
+++ ObjectComparer.Tests/ComparerFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ObjectComparer.Tests
+{
+    [TestClass]
+    public class ComparerFixture
+    {
+        [TestMethod]
+        public void Null_values_are_similar_test()
+        {
+            string first = null, second = null;
+            Assert.IsTrue(Comparer.AreSimilar(first, second));
+        }
+        [TestMethod]
+        public void Null_values_are_similar_test()
+        {
+            string first = null, second = null;
+            Assert.IsTrue(Comparer.AreSimilar(first, second));
+        }
+    }
+}
---
+++ ObjectComparer/Comparer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectComparer
+{
+    public static class Comparer
+    {
+        public static bool AreSimilar<T>(T first, T second )
+        {
+
+            if (first == null && second == null)
+                return false;
+            if (first.GetType() != second.GetType())
+                return false;
+
+            if (first.GetType().IsValueType && second.GetType().IsValueType && !first.Equals(second))
+                return false;
+
+
+
+
+
+
+
+            /// Add your implementation logic here. Feel free to add classes and types as required for your solution.
+            return true;
+        }
+    }
+}
